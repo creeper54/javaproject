0a36df5f44c204f4370f3fa19365607498f9952a
USER <USER@INET-16>
2018-03-23 10:47:45 +0900

    현홍이븅신

---
+++ 323/src/CharcterTest.java
@@ -0,0 +1,12 @@
+
+public class CharcterTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GaneCharacter charac = new GaneCharacter();
+		charac.add("Sword",1 ,100);
+		charac.add("Gun",2,50);
+		charac.print();
+	}
+
+}
