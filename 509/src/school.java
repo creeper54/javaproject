74bea04b2831b57400f67e5f497016f8c6771715
USER <USER@INET-05>
2018-05-09 19:50:31 +0900

    wdwqedwqwqewqeqdsadwqed

---
+++ 509/src/school.java
@@ -0,0 +1,33 @@
+
+public class school {
+	int classroom; // 교실
+	String Schoolname;// 학교명
+	String address;// 주소
+ }
+class Schoolplace{
+	String Schooloffice; // 교무실
+	String Administrativeoffice;// 행정실
+	String Principaloffice;// 교장실
+	String Nursingroom;// 보건실
+	String Practiceroom;// 실습실
+	String Playground; // 운동장
+	String auditorium;// 강당 
+    String library; // 도서관
+    String Consultationroom;//상담실
+    String pressroom;// 인쇄실   
+}
+class gwa{
+	String Cardepartment;
+	String WebContents;
+}
+class student{
+	String oneGrade;
+	String twoGrade;
+	String threeGrade;
+}
+class Teacher{
+	String Koreanteacher;
+	String Mathteacher;
+	String Scienceteacher;
+	
+}
---
+++ 509/src/schoolTest.java
@@ -0,0 +1,9 @@
+
+public class schoolTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
