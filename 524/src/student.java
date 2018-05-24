524b04f137c46ccb7ad95d395927189f7b462eca
USER <USER@INET-02>
2018-05-24 20:05:06 +0900

    758684754

---
+++ 524/src/student.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] score = new int[4];
+		int[] rank = new int[4];
+
+		Scanner sc = new Scanner(System.in);
+
+		for (int i = 0; i < 4; i++) {
+			rank[i] = 1;
+		}
+
+		for (int i = 0; i < 4; i++) {
+			System.out.print("학생" + (i + 1) + "의 점수 : ");
+			score[i] = sc.nextInt();
+		}
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				if (score[i] < score[j])
+					rank[i] = rank[i] + 1;
+			}
+		}
+
+		for (int i = 0; i < 4; i++) {
+			System.out.println("학생" + (i + 1) + "의 순위 : " + rank[i] + "등");
+		}
+	}
+}
---
+++ 524/src/subject.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class subject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+		int Korean, Math, Social, Science;
+		int s, c;
+ 
+		System.out.println("과목의 점수를 입력 하시오");
+
+		System.out.print("국어 점수를 입력하시오:");
+		Korean = input.nextInt();
+		System.out.print("수학 점수를 입력하시오:");
+		Math = input.nextInt();
+		System.out.print("사회 점수를 입력하시오:");
+		Social = input.nextInt();
+		System.out.print("과학 점수를 입력하시오:");
+		Science = input.nextInt();
+
+		s = (Korean + Math + Social + Science);
+		c = (s / 4);
+
+		System.out.println("총점은" + s);
+		System.out.println("평균은" + c);
+		if (c >= 80)
+			System.out.printf("A");
+		else if (c >= 70)
+			System.out.printf("B");
+		else if (c >= 50)
+			System.out.printf("C");
+		else if (c >= 40)
+			System.out.printf("D");
+		else
+			System.out.printf("F");
+
+		System.out.printf(" 학점 입니다.");
+
+	}
+
+}
+
